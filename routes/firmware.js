76f5c4ccf8e59b44a7936ec0c5cae9bd97e8c9fe
Shane Hoey <5053990+shanehoey@users.noreply.github.com>
2021-06-06 18:06:04 +1000

    minor fixes

--- routes/firmware.js
+++ routes/firmware.js
@@ -4,19 +4,17 @@ let express = require('express');
 router = module.exports = express.Router();
 
 // static
-router.use('/files/',express.static('files/firmware'));
+router.use(express.static('files/firmware'));
 
 // default firmware
 router.get('/:firmware', (req, res) => {
     
     var firmware = require('../files/json/firmware.json');
     let result = firmware.find(firmware => firmware.name.toLowerCase() === req.params.firmware.toLowerCase());
-   
+ 
     console.info('200        - ' + req.url );
-    console.info('REDIRECT   - ' + result.path );
+    console.info('REDIRECT   - ' + '/firmware/' + result.path );
     console.info('USER AGENT - ' + req.get('user-agent'));
-
-    res.redirect(301, + result.path);
+    res.redirect(301,'/firmware/' + result.path);
 
  });
-
